7a36a4f1271eefbf09a33e02d301fe2c8d25d4b8
marks1024 <1703790+marks1024@users.noreply.github.com>
2018-03-14 22:11:05 +0600

    Added gitignore

--- src/main/java/examplejaxrs/TestJaxRsService.java
+++ src/main/java/examplejaxrs/TestJaxRsService.java
@@ -2,15 +2,22 @@ package examplejaxrs;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/items")
 public class TestJaxRsService {
     
     @GET
-    public String getItems() {
-        return "JAX-RS: Hello, world";
+    public Response getItems() {
+        String r = "JAX-RS: Hello, world";
+        return Response.ok(r).build();
+    }
+    
+    @GET
+    @Path("{id}")
+    public Response getById(@PathParam("id") String id) {
+        String r = "JAX-RS: you requested item with id \"" + id + "\"";
+        return Response.ok(r).build();
     }
 }
